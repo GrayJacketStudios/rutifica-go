78df0885df00351e642c2b78584119ba4fc64feb
Sebastian Cristi <GrayJacketStudios@gmail.com>
2024-08-18 14:07:49 -0300

    Generar prueba unitaria para la funcion de obtener digito verificador
    Signed-off-by: Sebastian Cristi <GrayJacketStudios@gmail.com>

--- rutificador_test.go
+++ rutificador_test.go
@@ -1,1 +1,23 @@
 package rutificador
+
+import "testing"
+
+func TestObtenerDV(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected rune
+	}{
+		{"18622178", '8'},
+		{"11111111", '1'},
+		{"70360100", '6'},
+		{"22222222", '2'},
+		{"15304340", 'K'},
+		{"8367720", '1'},
+	}
+
+	for _, tc := range testCases {
+		if result := obtenerDV(tc.input); result != tc.expected {
+			t.Errorf("obtenerDV(%s) = %v; want %v", tc.input, result, tc.expected)
+		}
+	}
+}
